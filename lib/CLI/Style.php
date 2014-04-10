5050f1e183b027bf88f9424b5b43e52b4aace4fe
Jesse Donat <donatj@gmail.com>
2014-04-10 12:20:21 -0500

    CLI\Style Refactored. Made callable by instance. Corrections to escape codes.

--- lib/CLI/Style.php
+++ lib/CLI/Style.php
@@ -9,53 +9,104 @@ namespace CLI;
 /**
  * Class Style
  *
- * @method static bold($text, $args = null)
- * @method static black($text, $args = null)
- * @method static blue($text, $args = null)
- * @method static green($text, $args = null)
- * @method static cyan($text, $args = null)
- * @method static red($text, $args = null)
- * @method static purple($text, $args = null)
- * @method static brown($text, $args = null)
- * @method static light_gray($text, $args = null)
- * @method static normal($text, $args = null)
- * @method static dim($text, $args = null)
- * @method static dark_gray($text, $args = null)
- * @method static light_blue($text, $args = null)
- * @method static light_green($text, $args = null)
- * @method static light_cyan($text, $args = null)
- * @method static light_red($text, $args = null)
- * @method static light_purple($text, $args = null)
- * @method static yellow($text, $args = null)
- * @method static white($text, $args = null)
+ * @method bold($text, $args = null)
+ * @method dim($text, $args = null)
+ *
+ * @method normal($text, $args = null)
+ * @method black($text, $args = null)
+ * @method red($text, $args = null)
+ * @method green($text, $args = null)
+ * @method yellow($text, $args = null)
+ * @method blue($text, $args = null)
+ * @method magenta($text, $args = null)
+ * @method cyan($text, $args = null)
+ * @method lightGray($text, $args = null)
+ * @method darkGray($text, $args = null)
+ * @method lightRed($text, $args = null)
+ * @method lightGreen($text, $args = null)
+ * @method lightYellow($text, $args = null)
+ * @method lightBlue($text, $args = null)
+ * @method lightMagenta($text, $args = null)
+ * @method lightCyan($text, $args = null)
+ * @method white($text, $args = null)
  *
  * @package CLI
  */
 class Style {
 
-	static $foreground_colors = array(
-		'bold'       => '1',    'dim'          => '2',
-		'black'      => '0;30', 'dark_gray'    => '1;30',
-		'blue'       => '0;34', 'light_blue'   => '1;34',
-		'green'      => '0;32', 'light_green'  => '1;32',
-		'cyan'       => '0;36', 'light_cyan'   => '1;36',
-		'red'        => '0;31', 'light_red'    => '1;31',
-		'purple'     => '0;35', 'light_purple' => '1;35',
-		'brown'      => '0;33', 'yellow'       => '1;33',
-		'light_gray' => '0;37', 'white'        => '1;37',
-		'normal'     => '0;39',
+	const BOLD = 'bold';
+	const DIM  = 'dim';
+
+	const UNDERLINE = 'underline';
+	const BLINK     = 'blink';
+	const REVERSE   = 'reverse';
+	const HIDDEN    = 'hidden';
+
+	const NORMAL        = 'normal';
+	const BLACK         = 'black';
+	const RED           = 'red';
+	const GREEN         = 'green';
+	const YELLOW        = 'yellow';
+	const BLUE          = 'blue';
+	const MAGENTA       = 'magenta';
+	const CYAN          = 'cyan';
+	const LIGHT_GRAY    = 'lightGray';
+	const DARK_GRAY     = 'darkGray';
+	const LIGHT_RED     = 'lightRed';
+	const LIGHT_GREEN   = 'lightGreen';
+	const LIGHT_YELLOW  = 'lightYellow';
+	const LIGHT_BLUE    = 'lightBlue';
+	const LIGHT_MAGENTA = 'lightMagenta';
+	const LIGHT_CYAN    = 'lightCyan';
+	const WHITE         = 'white';
+
+	protected static $foreground_colors = array(
+		self::BOLD          => 1,
+		self::DIM           => 2,
+		self::NORMAL        => 39,
+		self::BLACK         => 30,
+		self::RED           => 31,
+		self::GREEN         => 32,
+		self::YELLOW        => 33,
+		self::BLUE          => 34,
+		self::MAGENTA       => 35,
+		self::CYAN          => 36,
+		self::LIGHT_GRAY    => 37,
+		self::DARK_GRAY     => 90,
+		self::LIGHT_RED     => 91,
+		self::LIGHT_GREEN   => 92,
+		self::LIGHT_YELLOW  => 93,
+		self::LIGHT_BLUE    => 94,
+		self::LIGHT_MAGENTA => 95,
+		self::LIGHT_CYAN    => 96,
+		self::WHITE         => 97,
 	);
 
-	static $background_colors = array(
-		'black' => '40', 'red' => '41',
-		'green' => '42', 'yellow' => '43',
-		'blue'  => '44', 'magenta' => '45',
-		'cyan'  => '46', 'light_gray' => '47',
+	protected static $background_colors = array(
+		self::NORMAL        => 49,
+		self::BLACK         => 40,
+		self::RED           => 41,
+		self::GREEN         => 42,
+		self::YELLOW        => 43,
+		self::BLUE          => 44,
+		self::MAGENTA       => 45,
+		self::CYAN          => 46,
+		self::LIGHT_GRAY    => 47,
+		self::DARK_GRAY     => 100,
+		self::LIGHT_RED     => 101,
+		self::LIGHT_GREEN   => 102,
+		self::LIGHT_YELLOW  => 103,
+		self::LIGHT_BLUE    => 104,
+		self::LIGHT_MAGENTA => 105,
+		self::LIGHT_CYAN    => 106,
+		self::WHITE         => 107,
 	);
 
-	static $options = array(
-		'underline' => '4', 'blink' => '5',
-		'reverse'   => '7', 'hidden' => '8',
+	protected static $options = array(
+		self::UNDERLINE => 4,
+		self::BLINK     => 5,
+		self::REVERSE   => 7,
+		self::HIDDEN    => 8,
 	);
 
 	public static function __callStatic( $foreground_color, $args ) {
@@ -85,6 +136,10 @@ class Style {
 
 		return $colored_string;
 
+	}
+
+	public function __call( $foreground_color, $args ) {
+		return self::__callStatic($foreground_color, $args);
 	}
 
 }
